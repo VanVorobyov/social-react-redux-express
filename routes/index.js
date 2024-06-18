29c7e6e66b8f1a57e8ca760e0e5a10bd5aedf814
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-06-18 21:22:02 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/register', function (req, res, next) {
+    res.send('Register');
+});
+
+
+module.exports = router;
