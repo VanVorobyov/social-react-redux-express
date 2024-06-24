ec644ba40198c53bb43d7accbb8df41438dc6b1e
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-06-24 22:44:40 +0300

    feat (routes): add user's routes

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
+// const multer = require('multer');
+const {UserController} = require("../controllers");
 
 
-// -------------------------------------------------- Где будем хранить файлы
-const uploadsDestination = 'uploads';
-const storage = multer.diskStorage({
-    destination: uploadsDestination,
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
-});
-
-const upload = multer({storage: storage});
-
-//--------------------------------------------------
+// // -------------------------------------------------- Где будем хранить файлы
+// const uploadsDestination = 'uploads';
+// const storage = multer.diskStorage({
+//     destination: uploadsDestination,
+//     filename: function (req, file, cb) {
+//         cb(null, file.originalname)
+//     }
+// });
+//
+// const upload = multer({storage: storage});
+//
+// //--------------------------------------------------
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('index', {title: 'Express'});
 });
 
-router.get('/register', function (req, res, next) {
-    res.send('Register');
-});
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+router.get('/current', UserController.currentUser);
+router.get('/users/:id', UserController.login);
+router.put('/users/:id', UserController.updateUser);
 
 
 module.exports = router;
