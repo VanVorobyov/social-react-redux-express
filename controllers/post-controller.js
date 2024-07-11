8c3c4656e54481829355b9fa173335799189ebd4
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-07-11 08:01:17 +0300

    feat (post-controller): add create post method

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -2,9 +2,37 @@ const {prisma} = require('../prisma/prisma-client');
 
 
 const PostController = {
+
     createPost: async (req, res) => {
-        res.send('create');
+        // Извлекаем содержание поста из тела запроса
+        const {content} = req.body;
+        // Получаем ID автора из пользовательских данных в запросе
+        const authorId = req.user.userId;
+
+        // Проверяем, заполнено ли содержание поста
+        if (!content) {
+            // Если нет, возвращаем ответ с ошибкой и статусом 400
+            return res.status(400).json({error: 'Все поля обязательные для заполнения'});
+        }
+
+        try {
+            // Пытаемся создать новый пост в базе данных с помощью Prisma
+            const post = await prisma.post.create({
+                data: {
+                    content,
+                    authorId
+                }
+            });
+            // Возвращаем созданный пост в ответе
+            res.json(post);
+        } catch (error) {
+            // Логируем ошибку в консоль
+            console.error("Error in CREATE POST ", error.message);
+            // Возвращаем ответ с ошибкой и статусом 500
+            res.status(500).json({error: 'Не удалось создать пост'});
+        }
     },
+
     getAllPosts: async (req, res) => {
         res.send('getAll');
     },
