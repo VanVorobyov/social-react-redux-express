91d45aa12d6e51e7b45e4e17a6991d8d1651cb7e
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-07-12 12:55:25 +0300

    feat(like-controller): add likePost method of LikeController

--- controllers/like-controller.js
+++ controllers/like-controller.js
@@ -2,7 +2,38 @@ const {prisma} = require("../prisma/prisma-client");
 
 const LikeController = {
     likePost: async (req, res) => {
-        res.send("likePost")
+        const {postId} = req.body;
+        const userId = req.user.userId;
+
+        if (!postId) {
+            return res.status(400).json({error: 'Все поля обязательные для заполнения'});
+        }
+
+        try {
+            const existingLike = await prisma.like.findFirst({
+                where: {
+                    postId,
+                    userId
+                }
+            })
+
+            if (existingLike) {
+                return res.status(400).json({error: 'Вы уже поставили лайк'});
+            }
+
+            const like = await prisma.like.create({
+                data: {
+                    postId,
+                    userId
+                }
+            })
+
+            res.json(like);
+        } catch (error) {
+            console.error("Error in LIKE POST", error.message);
+            res.status(500).json({error: 'Internal server error'});
+        }
+
     },
     dislikePost: async (req, res) => {
         res.send("dislikePost")
