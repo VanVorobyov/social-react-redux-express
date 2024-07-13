0807aeba339a0b1aaab271cc8979b2c6bb69d134
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-07-13 20:40:50 +0300

    feat(follow-controller): add basic follow-controller

---
+++ controllers/follow-controller.js
@@ -0,0 +1,13 @@
+const {prisma} = require("../prisma/prisma-client");
+
+
+const FollowController = {
+    followUser: async (req, res) => {
+
+    },
+    unfollowUser: async (req, res) => {
+
+    },
+}
+
+module.exports = FollowController
--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const UserController = require('./user-controller');
 const PostController = require('./post-controller');
 const LikeController = require('./like-controller');
 const CommentController = require('./comment-controller');
+const FollowController = require('./follow-controller');
 
 module.exports = {
     UserController,
     PostController,
     LikeController,
-    CommentController
+    CommentController,
+    FollowController
 }
