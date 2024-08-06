dbc80cd3fbe7c38fd0563ad1d667c21995b79a75
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-08-06 21:17:10 +0300

    feat(app): add upload images func

--- app.js
+++ app.js
@@ -8,24 +8,23 @@ require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
-
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.set('view engine', 'jade');
-// app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 
 app.use('/api', require('./routes'));
 
-// if (fs.existsSync('uploads')) {
-//     fs.mkdirSync('uploads');
-// }
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//     next(createError(404));
-// });
+if (fs.existsSync('uploads')) {
+  fs.mkdirSync('uploads');
+}
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function(err, req, res, next) {
